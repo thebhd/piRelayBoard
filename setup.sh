cffd0a068a58428fc6b80314f2818f827876daef
dan <dan@dayba.com>
2019-01-12 19:22:09 +0000

    updated setup with changes

--- setup.sh
+++ setup.sh
@@ -1,3 +1,16 @@
 #! /bin/bash
+echo "running piRelayBoard setup script"
+mkdir ~/DaybaScripts
+cd ~/DaybaScripts
+
 
 echo "copying template file to the pi"
+
+
+#copy short cut to the pi desktop
+cp  ~/DaybaScripts/piRelayBoard/Setup/piRelayBoardSetGPIO.desktop ~/Desktop/piRelayBoardSetGPIO.desktop
+
+#copy the examples to the desktop
+mkdir ~/Desktop/Scratch2Examples
+sudo cp ~/DaybaScripts/piRelayBoard/Setup/Scratch2Examples/* ~/Desktop/Scratch2Examples/
+

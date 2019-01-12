934680be93e78feb72d918b9479ba2b68fe7d326
dan <dan@dayba.com>
2019-01-12 19:39:32 +0000

    updated setupGPIO.sh to test relays

--- Setup/setGPIO.sh
+++ Setup/setGPIO.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+echo "Setting gpio pins as output"  
 gpio mode 26 out
 gpio mode 22 out
 gpio mode 23 out
@@ -7,3 +8,33 @@ gpio mode 24 out
 gpio mode 28 out
 gpio mode 25 out
 gpio mode 29 out
+
+echo "all relays should be on "
+echo "switching off each led in tern"
+gpio wirte 22 1
+sleep 0.5
+gpio write 26 1
+sleep 0.5
+gpio write 23 1
+sleep 0.5
+gpio write 24 1
+sleep 0.5
+gpio write 27 1
+sleep 0.5
+gpio write 25 1
+sleep 0.5
+gpio write 28 1
+sleep 0.5
+gpio write 29 1
+
+echo "all relays should be off now "
+echo ""
+echo ""
+echo "=========== ! important ==================="
+echo "|     these are low active relays         |"
+echo "|     meaing the output must be set to    |"
+echo "|     0 or low to switch the realay on    |"
+echo "==========================================="
+echo ""
+echo ""
+read -n 1 -s -r -p "This window is not now needed press any key to exit"
